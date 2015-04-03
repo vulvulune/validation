8a2e8239bd3558e656c2f3016922ceb87639b020
janvanderhaegen <van.der.haegen.j@gmail.com>
2015-04-02 20:46:18 -0500

    doc(sample): updated after async changes
    Added example of 'passes' with a Promise

--- src/examples/validators/passes/view-model.js
+++ src/examples/validators/passes/view-model.js
@@ -4,9 +4,22 @@ export class Person {
   constructor(validation) {
     this.passesProperty = '333-22-4444';
 
+    this.userNameProperty = '';
+
     this.validation = validation.on(this)
       .ensure('passesProperty')
         .passes((newValue) => { return /^\d{3}-\d{2}-\d{4}$/.test(newValue); })
-        .withMessage("Not a valid SSN");
+        .withMessage("Not a valid SSN")
+      .ensure('userNameProperty')
+        .passes(
+          (newValue) => {
+            //Simulate delay "due to service call"
+            return new Promise( (accept, reject) => {
+              setTimeout(()=> {
+                reject("username already taken");
+              }, 1500);
+            });
+          }
+        );
   }
 }
